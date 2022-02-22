879260144e5a7d448467ecab31d3a7e623eebb64
Luke <lnt511@york.ac.uk>
2022-02-22 23:02:24 +0000

    Added debug function

--- board_handling/feature_detection.py
+++ board_handling/feature_detection.py
@@ -34,6 +34,12 @@ def sift_get_keypoints_descriptors(img, draw=False):
     
     return keypoints, descriptors
 
+def draw_matches(possiblyCorrectMatches, source_image, source_keypoints, target_image, target_keypoints):
+    img_matched = cv2.drawMatchesKnn(source_image, source_keypoints, target_image, target_keypoints, possiblyCorrectMatches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    fig=plt.figure(figsize=(24,24)) 
+    plt.imshow(img_matched)
+    plt.show()
+
 def main(source_file = "assets\\3.1 Blue-Yellow,Green,Gray\\PXL_20220209_153108922.jpg", target_file = "assets\\0.0 Cropped\\11.png"):
     source_img = cv2.imread(source_file, 1)
     source_img = cv2.cvtColor(source_img, cv2.COLOR_BGR2RGB)
@@ -62,23 +68,27 @@ def main(source_file = "assets\\3.1 Blue-Yellow,Green,Gray\\PXL_20220209_1531089
     
     np.random.shuffle(possiblyCorrectMatches)
     
-    # numMatches = len(possiblyCorrectMatches)
-    # print(possiblyCorrectMatches.shape)
-    # print(targetpts.shape)
-    # print(sourcepts.shape)
+    print(possiblyCorrectMatches.shape)
+    print(targetpts.shape)
+    print(sourcepts.shape)
     
     H = cv2.findHomography(sourcepts, targetpts)[0]
     
-    # for i in range(numMatches):
-    #     xy = H @ np.array([[sourcepts[i,0]], [sourcepts[i,1]], [1]])
-    #     x = xy[0]/xy[2]
-    #     y = xy[1]/xy[2]
-    #     print(f"\n{x[0]},{y[0]}")
-    #     print(targetpts[i])
+    draw_matches(possiblyCorrectMatches, source_img, source_keypoints, target_img, target_keypoints)
+    numMatches = len(possiblyCorrectMatches)
+    for i in range(numMatches):
+        xy = H @ np.array([[sourcepts[i,0]], [sourcepts[i,1]], [1]])
+        x = xy[0]/xy[2]
+        y = xy[1]/xy[2]
+        print(f"\n{x[0]},{y[0]}")
+        print(targetpts[i])
     
     warped = cv2.warpPerspective(source_img, H, (target_img.shape[1], target_img.shape[0]))
     plt.imshow(warped)
     plt.show()
 
 if __name__ == "__main__":
-    main(source_file="assets\\2.0 Red-Red\\PXL_20220209_150018426.jpg")
+    main(
+        target_file="assets\\0.0 Cropped\\11.png",
+        source_file="assets\\2.0 Red-Red\\PXL_20220209_150018426.jpg"
+        )
