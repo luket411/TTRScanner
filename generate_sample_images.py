f925816bbfd450c7a9de5c9f1920c4af7ca5dafc
Luke <lnt200011@gmail.com>
2022-02-25 16:46:21 +0000

    Added journeys.csv

--- generate_sample_images.py
+++ generate_sample_images.py
@@ -3,15 +3,20 @@ import cv2
 from train_detection.show_train_blocks import get_train_segments
 import matplotlib.pyplot as plt
 from util.timer import timer
+from board_handling.feature_detection import find_board
+
 
 @timer
 def generate_sample_images(output_dir, image_file, train_data_file):
     
     train_segments = get_train_segments(train_data_file)
     
-    img = cv2.imread(image_file, 1)
-    img = cv2.cvtColor(img, 4)
-    
+    # img = cv2.imread(image_file, 1)
+    # img = cv2.cvtColor(img, 4)
+    base_file = "assets/0.0 Cropped/11.png"
+    img = find_board(base_file, image_file)
+
+
     out_string_colours = ""
     
     for i, segment in enumerate(train_segments):
@@ -32,6 +37,6 @@ def generate_sample_images(output_dir, image_file, train_data_file):
 
 generate_sample_images(
     "assets/general/base",
-    "assets/0.0 Cropped/11.png",
+    "assets/2.1 Red-Yellow,Green,Gray/PXL_20220209_151741865.jpg",
     "assets/0.0 Cropped/trains11.csv"
 )
