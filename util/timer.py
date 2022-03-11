1dd67e6b081d8e75d852a5d65657ef5e4e25b372
Luke <lnt200011@gmail.com>
2022-03-11 00:56:53 +0000

    Added util features

--- util/timer.py
+++ util/timer.py
@@ -1,11 +1,20 @@
+from sys import path
+from os import path as ospath
+path.append(ospath.join(ospath.dirname(__file__), ".."))
+
 from time import time
+
+from util.constants import ENABLE_TIMER
 
 def timer(func):
     def wrapper(*args, **kwargs):
-        t = time()
-        val = func(*args, **kwargs)
-        t = time() - t
-        print(f"Time Taken: {t}s")
+        if ENABLE_TIMER:
+            t = time()
+            val = func(*args, **kwargs)
+            t = time() - t
+            print(f"Time Taken: {t}s")
+        else:
+            val = func(*args, **kwargs)
         return val
 
     return  wrapper
